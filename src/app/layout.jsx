dcf6fe10e5ee8a535f11705560a9be54ec202172
sakshiS6 <sakshimishra2004545@gmail.com>
2025-01-30 12:20:32 +0530

    Changed the font family

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,10 +1,11 @@
-import { Inter } from "next/font/google";
+import { Outfit , Ovo } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "react-hot-toast";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 
-const inter = Inter({ subsets: ["latin"] });
+const outfit = Outfit({ subsets: ["latin"] , weight: ["400" ,"500" ,"600" ,"700"]});
+const ovo = Ovo({ subsets: ["latin"] , weight: ["400"] });
 
 export const metadata = {
   title: "Create Next App",
@@ -15,7 +16,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={inter.className}
+        className={`${outfit.className} ${ovo.className} antialiased`}
       >
         
           <Navbar />
