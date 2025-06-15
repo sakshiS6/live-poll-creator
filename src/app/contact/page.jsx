ba06628525138a7706ce335cd6754d798e6f8901
sakshiS6 <sakshimishra2004545@gmail.com>
2025-06-15 16:14:30 +0530

    Updated

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -5,8 +5,8 @@ const Contact = () => {
     <div>
       <>
         {/* Contact Us */}
-        <div className="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto bg-violet-300">
-          <div className="max-w-2xl lg:max-w-5xl mx-auto">
+        <div className="max-w-100% px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto bg-violet-300">
+          <div className="max-w-2xl lg:max-w-5xl mx-auto bg-white px-4 py-2 rounded-xl">
             <div className="text-center">
               <h1 className="text-3xl font-bold text-black sm:text-4xl dark:text-white">
                 Contact us
